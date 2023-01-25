a956ca9f789702d63421479fb553c889a7eb8938
essyo4 <estherkathmbi46@gmail.com>
2023-01-25 13:27:37 -0800

    python

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -15,4 +15,4 @@ def safe_print_integer_err(value):
         return (True)
     except (TypeError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (False)
+    return (False)
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -14,4 +14,4 @@ def safe_function(fct, *args):
         return (result)
     except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+    return (None)
